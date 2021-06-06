b67b592d368ffe291d9453a5f84454e3b08f4756
Valevach.90 <Valevach.90@mail.ru>
2021-06-06 19:12:06 +0300

    06.06.2021

--- src/main/java/com/efimchick/ifmo/collections/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/collections/countwords/Words.java
@@ -8,7 +8,7 @@ public class Words {
     public String countWords(List<String> lines) {
         Map<String, Integer> wordMap = new HashMap<>();
         for (String line : lines) {
-            String[] words = line.toLowerCase().split("(?U)[\\pP\\s]");
+            String[] words = line.toLowerCase().split("(?U)[\\pP\\s\\d+]");
             for (int i = 0; i < words.length; i++) {
                 if (words[i].length() >= 4) {
                     if (wordMap.get(words[i]) == null) {
@@ -24,20 +24,29 @@ public class Words {
             @Override
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
                 if (o2.getValue() == o1.getValue()) {
-                    String s1 = o1.getKey().toLowerCase();
-                    String s2 = o2.getKey().toLowerCase();
+                    String s1 = o1.getKey();
+                    String s2 = o2.getKey();
                     int i = 0;
                     while (i < s1.length() && i < s2.length()) {
+
                         if (s1.charAt(i) > s2.charAt(i)) {
                             return 1;
+
                         } else if (s1.charAt(i) < s2.charAt(i)) {
                             return -1;
-                        } else {
+
+                        } else if (i == s1.length() - 1 ) {
+
+                            return -1;
+                        } else if (i == s2.length() - 1 ) {
+
+                            return 1;
+
+                        }else {
                             i++;
                         }
 
                     }
-
                 }
                 return o2.getValue() - o1.getValue();
 
@@ -46,9 +55,9 @@ public class Words {
         StringBuilder result = new StringBuilder();
         for (Map.Entry<String, Integer> entry : sortedWords) {
             if (entry.getValue() > 10 && result.length() == 0) {
-                result.append(entry.getKey().toLowerCase() + " - " + entry.getValue());
+                result.append(entry.getKey() + " - " + entry.getValue());
             } else if (entry.getValue() >= 10) {
-                result.append("\n" + entry.getKey().toLowerCase() + " - " + entry.getValue());
+                result.append("\n" + entry.getKey() + " - " + entry.getValue());
             }
         }
 
